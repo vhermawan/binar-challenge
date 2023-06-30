a001518ecae1d85519a0fa66f730a8e4345196b7
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2023-06-30 14:07:34 +0700

    feat: add search car done

--- src/common/API/index.js
+++ src/common/API/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL
-
 export const API = {
-  get : async function (endPoint) {      
-    return await axios.get(API_URL + endPoint)
+  get : async function (endPoint) {   
+    let headers = {}
+    headers.Access_token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY4ODA5NDk0MH0.RA4WX_Tvd6TzI3TkDWxi7v0yj8DAore6e22k37-_HJ0`   
+    return await axios.get('https://api-car-rental.binaracademy.org/' + endPoint,  { headers: headers })
     .then(response => {
       return response;
     }).catch(error => {
